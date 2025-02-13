67e0da218c155063f8580cbc5d0e1e5f94fc989e
Joseap1996 <joseap1996@live.com>
2025-02-12 22:25:28 -0600

    D:move right and left

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ def main():
        if event.type == pygame.QUIT:
           return
 
+     # limit the framerate to 60fps
+     dt = clock.tick(60) / 1000
+     player.update(dt)
      screen.fill("black")
      player.draw(screen)
      pygame.display.flip()
-
-     # limit the framerate to 60fps
-     dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
